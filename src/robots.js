517d504a815018b42856631c8d15ea3bb38aa479
PeacockCo <pavaoleonardo@hotmail.com>
2018-11-08 11:49:20 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leonardo Silva Pavao',
+    username: 'Bret',
+    email: 'pavaoleonardo@hotmail.biz'
+  },
+  {
+    id: 2,
+    name: 'Liam Silva Zaplitnny',
+    username: 'Antonette',
+    email: 'pavaoliam@gmail.net'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Alina Zaplitna',
+    username: 'Karianne',
+    email: 'alizap23@msn.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
